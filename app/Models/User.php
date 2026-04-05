cd514958661dcbfb9d6bd7d436ab03431d95aed4
Wojciech Rybinski <wojciech.rybinski@displate.com>
2026-04-05 22:38:22 +0200

    feat(auth): add google_id column and make password nullable

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'tenant_id',
+        'google_id',
     ];
 
     public function isAdmin(): bool
